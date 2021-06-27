dd3f35ff7af98ccc262815db716c513c86f00eb1
swordfall <2506597416@qq.com>
2021-06-28 00:47:51 +0800

    CountDownLatch类的sync内部类及其tryAcquireShared和tryReleaseShared实现，CountDownLatch带有count构造函数，常用方法await和countDown，常用方法其实调用的是sync类的共享模式方法

--- java/util/concurrent/CountDownLatch.java
+++ java/util/concurrent/CountDownLatch.java
@@ -152,6 +152,96 @@ import java.util.concurrent.locks.AbstractQueuedSynchronizer;
  *
  * @since 1.5
  * @author Doug Lea
+ *
+ * CountDownLatch是并发包中的一个工具类，它的典型应用场景为：一个线程等待几个线程执行，
+ * 待这几个线程结束后，该线程再继续执行。
+ *
+ * 简单起见，可以把它理解为一个倒数的计时器：初始值为线程数，每个线程结束时执行减1操作，
+ * 当计数器减到0时等待的线程再继续执行。
+ *
+ * 场景举例：
+ * 场景1：一个线程等待多个线程执行完之后再继续执行
+ *
+ *  public void test() throws InterruptedException{
+ *      int count = 5;
+ *      // CountDownLatch 的初始化计数器为 5
+ *      // 注意线程数和计数器保持一致
+ *      CountDownLatch countDownLatch = new CountDownLatch(count
+ *      for （int i = 0; i < count; i++){
+ *          int finalI = i;
+ *          new Thread(() -> {
+ *              try{
+ *                  TimeUnit.SECONDS.sleep(finalI);
+ *              }catch (InterruptedException e){
+ *                  e.printStackTrace();
+ *              }
+ *              System.out.println(Thread.currentThread().getName() + " is working ...");
+ *              // 每个线程执行结束时执行 countDown
+ *              countDownLatch.countDown();
+ *          }).start();
+ *      }
+ *      // 主线程进入等待状态（尝试获取资源，成功后才能继续执行）
+ *      countDownLatch.await();
+ *      System.out.println(Thread.currentThread().getName() + " go on ...");
+ *  }
+ *
+ *  输出结果：
+ *      Thread-0 is working ...
+ *      Thread-1 is working ...
+ *      Thread-2 is working ...
+ *      Thread-3 is working ...
+ *      Thread-4 is working ...
+ *      main go on ...
+ *
+ *  场景2：一个线程到达指定条件后，通知另一个线程
+ *  private static volatile List<Integer> list = new ArrayList<>();
+ *
+ *  private static void test() {
+ *      CountDownLatch countDownLatch = new CountDownLatch(1);
+ *
+ *      new Thread(() -> {
+ *          if (list.size != 5) {
+ *              try{
+ *                  // list 的大小为 5 时再继续执行，否则等待
+ *                  // 等待 state 减到 0
+ *                  countDownLatch.await();
+ *              }catch (InterruptedException e){
+ *                  e.printStackTrace();
+ *              }
+ *          }
+ *          System.out.println(Thread.currentThread().getName() + " start ...");
+ *      }).start();
+ *
+ *      new Thread(() -> {
+ *          for(int i = 0; i < 10; i++){
+ *              list.add(i);
+ *              System.out.println(Thread.currentThread().getName() + " add " + i);
+ *              if(list.size() == 5){
+ *                  // 满足条件时将 state 减 1
+ *                  countDownLatch.countDown();
+ *              }
+ *              try{
+ *                  TimeUnit.SECONDS.sleep(1);
+ *              }catch(InterruptedException e){
+ *                  e.printStackTrace();
+ *              }
+ *          }
+ *      }).start();
+ *  }
+ *
+ *  输出结果：
+ *      Thread-1 add 0
+ *      Thread-1 add 1
+ *      Thread-1 add 2
+ *      Thread-1 add 3
+ *      Thread-1 add 4
+ *      Thread-0 start ...
+ *      Thread-1 add 5
+ *      Thread-1 add 6
+ *      Thread-1 add 7
+ *      Thread-1 add 8
+ *      Thread-1 add 9
+ *
  */
 public class CountDownLatch {
     /**
@@ -161,6 +251,7 @@ public class CountDownLatch {
     private static final class Sync extends AbstractQueuedSynchronizer {
         private static final long serialVersionUID = 4982264981922014374L;
 
+        // 构造器，初始化AQS的 state 变量
         Sync(int count) {
             setState(count);
         }
@@ -169,22 +260,34 @@ public class CountDownLatch {
             return getState();
         }
 
+        // 尝试获取资源的操作，只有当 state 变量为0 的时候才能获取成功（返回 1）
         protected int tryAcquireShared(int acquires) {
             return (getState() == 0) ? 1 : -1;
         }
 
+        // 尝试释放资源的操作
         protected boolean tryReleaseShared(int releases) {
             // Decrement count; signal when transition to zero
+            // 死循环
             for (;;) {
                 int c = getState();
                 if (c == 0)
                     return false;
+                // 该操作就是尝试把 state 变量减去 1
                 int nextc = c-1;
                 if (compareAndSetState(c, nextc))
                     return nextc == 0;
             }
         }
     }
+
+    /**
+     * Sync 继承了AQS抽象类，根据AQS可知，acquireSharedInterruptibly和tryAcquireSharedNanos方法的实现
+     * 都调用了tryAcquireShared。
+     *
+     * 流程说明：通常先把CountDownLatch的计数器(state)初始化为N，执行await操作就是尝试以共享模式获取资源，
+     * 而每次countDown操作就是将 N 减去 1，只有当 N 减到 0 的时候，才能获取成功(tryAcquireShard方法)，然后继续执行。
+     */
 
     private final Sync sync;
 
@@ -194,11 +297,24 @@ public class CountDownLatch {
      * @param count the number of times {@link #countDown} must be invoked
      *        before threads can pass through {@link #await}
      * @throws IllegalArgumentException if {@code count} is negative
+     *
+     * 构造器（该构造器是唯一的）传入一个正整数，且初始化了sync变量，Sync是内部的一个嵌套类，
+     * 继承自AQS
      */
     public CountDownLatch(int count) {
         if (count < 0) throw new IllegalArgumentException("count < 0");
         this.sync = new Sync(count);
     }
+
+    /**
+     * 常用方法：
+     * 1. await();
+     * 2. await(long, TimeUnit);
+     * 3. countDown.
+     * 其中两个await都是让当前线程进入等待状态（获取资源失败）；
+     * 而countDown方法是将计数器减去1，当计数器为0的时候，那些处于
+     * 等待状态的线程会继续执行（获取资源成功）
+     */
 
     /**
      * Causes the current thread to wait until the latch has counted down to
@@ -226,6 +342,8 @@ public class CountDownLatch {
      *
      * @throws InterruptedException if the current thread is interrupted
      *         while waiting
+     *
+     * await()调用acquireSharedInterruptibly，它是AQS中【共享模式】的方法，出现异常会响应中断
      */
     public void await() throws InterruptedException {
         sync.acquireSharedInterruptibly(1);
@@ -271,6 +389,9 @@ public class CountDownLatch {
      *         if the waiting time elapsed before the count reached zero
      * @throws InterruptedException if the current thread is interrupted
      *         while waiting
+     *
+     * await(long timeout, TimeUnit unit)调用tryAcquireSharedNanos，它是AQS中【共享模式】的常用方法，
+     * 出现异常会响应中断，并且有超时等待
      */
     public boolean await(long timeout, TimeUnit unit)
         throws InterruptedException {
